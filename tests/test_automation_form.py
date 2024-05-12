ec30fdf66376a18e3064dc1dafc4ff31d8e8bc3e
Dzhalal Atabaev <djaldoit@gmail.com>
2024-05-12 10:56:16 +0300

    test_automation finished

--- tests/test_automation_form.py
+++ tests/test_automation_form.py
@@ -2,14 +2,15 @@ from selene import have, be, browser
 import time
 
 def test_automation_form():
-    browser.open('/')
+    browser.open('/automation-practice-form')
+
     # Name, Last name, Email, Gender
     browser.element('#firstName').type('Piter')
     browser.element('#lastName').type('Parker')
     browser.element('#userEmail').type('spiderman@example.com')
     browser.element('#genterWrapper').element('.custom-control-label').click()
 
-    # Date of Birth, Phone Number, Subjects, Hobbies
+    # Phone Number, Date of Birth
     browser.element('#userNumber').type('1234567890')
     browser.element('#dateOfBirthInput').click()
     browser.element('.react-datepicker__month-select').element('option[value="6"]').click()
@@ -18,18 +19,33 @@ def test_automation_form():
 
     # Subjects, Hobbies
     browser.element('#subjectsInput').type('Arts').press_enter()
-    # browser.element('#hobbiesWrapper').element('[value=Sports]').click()
-    # browser.element('#hobbiesWrapper').all('type*=checkbox')[2].click()
+    browser.element('.//label[@for="hobbies-checkbox-1"]').click()
+    browser.element('.//label[@for="hobbies-checkbox-3"]').click()
 
-    # Picture, State and City, Address
-    browser.element('#uploadPicture').send_keys('C:/Users/djald/PycharmProjects/qa_guru_13_task5/picture/111.jpg')
+    # Picture, Address, State and City
+    browser.element('#uploadPicture').send_keys('C:/Users/djald/PycharmProjects/qa_guru_13_task5/images/111.jpg')
     browser.element('#currentAddress').type('Vladimir Street 12')
-    browser.element('#state').click().press('PageDown').press('Enter')
-    # browser.element('#city').click().element('[value=Karnal]').click()
-    time.sleep(10)
+    browser.element('#state').click().element('#react-select-3-option-2').click()
+    browser.element('#city').click().element('#react-select-4-option-0').click()
 
     # Submit
     browser.element('#submit').press_enter()
+
+    # Check
+    assert browser.element('.modal-content').should(have.text('Thanks for submitting the form'))
+    assert browser.element('.modal-content').should(have.text('Piter Parker'))
+    assert browser.element('.modal-content').should(have.text('spiderman@example.com'))
+    assert browser.element('.modal-content').should(have.text('Male'))
+    assert browser.element('.modal-content').should(have.text('1234567890'))
+    assert browser.element('.modal-content').should(have.text('28 June,1995'))
+    assert browser.element('.modal-content').should(have.text('Arts'))
+    assert browser.element('.modal-content').should(have.text('Sports, Music'))
+    assert browser.element('.modal-content').should(have.text('111.jpg'))
+    assert browser.element('.modal-content').should(have.text('Vladimir Street 12'))
+    assert browser.element('.modal-content').should(have.text('Haryana Karnal'))
+
+    #Close
+    browser.element('#closeLargeModal').click()
 
 
 
